696e9c1210be9eb178c528ed96e91e6c50f4ada6
Katerina Zakova <zakova.kz@gmail.com>
2023-11-22 10:00:10 +0100

    feat(VizitkaRepository.java): create repository Vizitka

--- src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
@@ -1,13 +1,19 @@
 package cz.czechitas.java2webapps.ukol6.controller;
 
+import cz.czechitas.java2webapps.ukol6.repository.VizitkaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class VizitkaController {
+    private final VizitkaRepository vizitkaRepository;
 
-   @GetMapping("/")
+    public VizitkaController(VizitkaRepository vizitkaRepository) {
+        this.vizitkaRepository = vizitkaRepository;
+    }
+
+    @GetMapping("/")
     public ModelAndView seznam(){
         ModelAndView modelAndView = new ModelAndView("seznam");
         return modelAndView;
